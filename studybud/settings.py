19ff8ef6ce440b46b8bda29b73f3c947632aa9d0
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-23 11:04:47 +0530

    created tamplates and changed folder structure

--- studybud/settings.py
+++ studybud/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -43,7 +45,9 @@ ROOT_URLCONF = 'studybud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
